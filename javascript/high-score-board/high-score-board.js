e49d04ce47beaa272d0ee9c1d39eec90bb4380b4
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-06-26 14:14:40 -0300

    feat: High Score Board third function

--- javascript/high-score-board/high-score-board.js
+++ javascript/high-score-board/high-score-board.js
@@ -31,7 +31,11 @@ export function addPlayer(scoreBoard, player, score) {
  * @returns {Record<string, number>} updated score board
  */
 export function removePlayer(scoreBoard, player) {
-  throw new Error("Please implement the removePlayer function");
+  if (scoreBoard.hasOwnProperty(player)) {
+    delete scoreBoard[player];
+    return scoreBoard;
+  }
+  return scoreBoard;
 }
 
 /**
