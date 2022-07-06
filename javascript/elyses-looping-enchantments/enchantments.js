174106e5df566dc9cc65f763b74dfe0819f24a55
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-07-06 09:00:14 -0300

    feat: Function 1 solution

--- javascript/elyses-looping-enchantments/enchantments.js
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,13 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0;
+  stack.forEach((number) => {
+    if (number === card) {
+      count += 1;
+    }
+  });
+  return count;
 }
 
 /**
@@ -20,5 +26,5 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+  throw new Error("Implement the determineOddEvenCards function");
 }
