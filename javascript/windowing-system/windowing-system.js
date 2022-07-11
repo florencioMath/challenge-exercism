4d02e7af912a3eed27071cbc1a7ea4da6e2e08a1
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-07-11 09:45:28 -0300

    feat: Function 2 solution

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -6,6 +6,7 @@
  * you defined so they are available for the tests.
  */
 
+//  1. Define Size for storing the dimensions of the window
 export function Size(width = 80, height = 60) {
   this.width = width;
   this.height = height;
@@ -14,4 +15,15 @@ export function Size(width = 80, height = 60) {
 Size.prototype.resize = function (newWidth, newHeight) {
   this.width = newWidth;
   this.height = newHeight;
+};
+
+// 2. Define Position to store a window position
+export function Position(x = 0, y = 0) {
+  this.x = x;
+  this.y = y;
+}
+
+Position.prototype.move = function (newX, newY) {
+  this.x = newX;
+  this.y = newY;
 };
