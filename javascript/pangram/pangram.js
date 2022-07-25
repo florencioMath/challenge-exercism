678435551c138e3fc1ba5830374a9d1fd7d23c34
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-07-25 20:21:37 -0300

    feat: Pangram solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,6 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+export const isPangram = (pharse) => {
+  pharse = pharse.toLowerCase();
+  return alphabet.every((letter) => pharse.includes(letter));
 };
