bf9d608e9ca9fc86f18605274e422f92b57bf68c
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-04-15 19:45:30 -0300

    feat: Vehicle Purchase.

--- javascript/javascript-challenges.js
+++ javascript/javascript-challenges.js
@@ -135,3 +135,31 @@
 // }
 
 // *****************************************************************************//
+
+// Vehicle Purchase
+// export function needsLicense(kind) {
+//   if(kind === 'car' || kind === 'truck'){
+//     return true;
+//   }
+//   return false;
+// }
+//
+// export function chooseVehicle(option1, option2) {
+//   if(option1 <= option2) {
+//     return option1 + ' is clearly the better choice.';
+//   } else {
+//     return option2 + ' is clearly the better choice.';
+//   }
+// }
+//
+// export function calculateResellPrice(originalPrice, age) {
+//   if(age < 3) {
+//     return (80 / 100) * originalPrice;
+//   } else if(age >= 3 && age <= 10) {
+//     return (70 / 100) * originalPrice;
+//   } else {
+//     return (50 / 100) * originalPrice;
+//   }
+// }
+
+// *****************************************************************************//
