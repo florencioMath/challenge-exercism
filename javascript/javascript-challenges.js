b2936fae2045f4492e80e1a09760473735ff0e09
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-04-28 14:49:04 -0300

    feat: Amusement Park

--- javascript/javascript-challenges.js
+++ javascript/javascript-challenges.js
@@ -280,3 +280,30 @@
 //  }
 
 // *****************************************************************************//
+
+// Amusement Park
+// export function createVisitor(name, age, ticketId) {
+//   return {name: name, age: age, ticketId: ticketId};
+// }
+//
+// export function revokeTicket(visitor) {
+//   visitor["ticketId"] = null;
+//   return visitor;
+// }
+//
+// export function ticketStatus(tickets, ticketId) {
+//   if(tickets[ticketId] === null) return "not sold";
+//
+//   if(tickets[ticketId] === undefined) return "unknown ticket id";
+//   if(tickets[ticketId] === tickets[ticketId]) return "sold to " + tickets[ticketId];
+// }
+//
+// export function simpleTicketStatus(tickets, ticketId) {
+//   return tickets[ticketId] ?? "invalid ticket !!!";
+// }
+//
+// export function gtcVersion(visitor) {
+//   return visitor.gtc?.version;
+// }
+
+// *****************************************************************************//
