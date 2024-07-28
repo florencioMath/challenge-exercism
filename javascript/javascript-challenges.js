b4aa86ec33006d17efdcbd4d95d3bc547373b9a1
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-07-28 20:36:13 -0300

    feat: Custom Signs.

--- javascript/javascript-challenges.js
+++ javascript/javascript-challenges.js
@@ -604,3 +604,23 @@
 // }
 
 // *****************************************************************************//
+
+// Custom Signs
+// export function buildSign(occasion, name) {
+//   return `Happy ${occasion} ${name}!`;
+// }
+//
+// export function buildBirthdaySign(age) {
+//   return `Happy Birthday! What a ${age >= 50 ? 'mature' : 'young'} fellow you are.`;
+// }
+//
+// export function graduationFor(name, year) {
+//   return `Congratulations ${name}!\nClass of ${year}`;
+// }
+//
+// export function costOf(sign, currency) {
+//   const SIGN_PRICE = (sign.length * 2 + 20).toFixed(2);
+//   return `Your sign costs ${SIGN_PRICE} ${currency}.`
+// }
+
+// *****************************************************************************//
