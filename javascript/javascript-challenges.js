bfa1c3e396bcd5b340161a4fcd5617f499ccd3bc
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-08-03 19:47:25 -0300

    feat: Translation Service.

--- javascript/javascript-challenges.js
+++ javascript/javascript-challenges.js
@@ -753,3 +753,70 @@
 // }
 
 // *****************************************************************************//
+
+// Translation Service
+// export class TranslationService {
+//
+//   constructor(api) {
+//     this.api = api;
+//   }
+//
+//   free(text) {
+//     return this.api.fetch(text).then(function (response) {
+//       return response.translation;
+//     }).catch(function (error) {
+//         throw error;
+//     });
+//   }
+//
+//   batch(texts) {
+//     if(texts.length === 0) {
+//       return Promise.reject(new BatchIsEmpty());
+//     }
+//     const freePromises = texts.map(text => this.free(text));
+//     return Promise.all(freePromises)
+//       .then(function(responses) {
+//         return responses;
+//       })
+//       .catch(function(error) {
+//         return Promise.reject(error);
+//       });
+//   }
+//
+//   request(text) {
+//     const newRequest = () => new Promise((resolve, reject) => {
+//       this.api.request(text, (result) => {
+//         result ? reject(result) : resolve();
+//       })
+//     })
+//     return newRequest().catch(newRequest).catch(newRequest);
+//   }
+//
+//   premium(text, minimumQuality) {
+//     return this.api.fetch(text)
+//       .catch(() => {
+//         return this.request(text).then(() => this.api.fetch(text));
+//       })
+//       .then((response) => {
+//         if(response.quality < minimumQuality) {
+//           throw new QualityThresholdNotMet();
+//         }
+//         return response.translation;
+//       })
+//   }
+// }
+//
+// export class QualityThresholdNotMet extends Error {
+//   constructor(text) {
+//     super(`The translation of ${text} does not meet the requested quality threshold.`.trim(),);
+//     this.text = text;
+//   }
+// }
+//
+// export class BatchIsEmpty extends Error {
+//   constructor() {
+//     super(`Requested a batch translation, but there are no texts in the batch.`.trim(),);
+//   }
+// }
+
+// *****************************************************************************//
