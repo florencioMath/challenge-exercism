5ca0fb0e0af1559139626f377a250bb914116e8e
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-08-12 20:42:55 -0300

    feat: Space Age.

--- javascript/javascript-challenges.js
+++ javascript/javascript-challenges.js
@@ -933,3 +933,26 @@
 //
 
 // *****************************************************************************//
+
+// Space Age
+// export const age = (planet, seconds) => {
+//   const earthYearInSeconds = 31557600;
+//   const orbitalPeriod = ORBITALPERIODS[planet];
+//   const ageInYears = seconds / (earthYearInSeconds * orbitalPeriod);
+//   const age = Number(ageInYears.toFixed(2));
+//
+//   return age;
+// };
+//
+// export const ORBITALPERIODS = {
+//     mercury: 0.2408467,
+//     venus: 0.61519726,
+//     earth: 1.0,
+//     mars: 1.8808158,
+//     jupiter: 11.862615,
+//     saturn: 29.447498,
+//     uranus: 84.016846,
+//     neptune: 164.79132
+// };
+
+// *****************************************************************************//
